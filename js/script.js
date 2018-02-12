2d5d91722bd9ffdb303a5749612d695aa266f1be
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-02-12 13:10:02 -0800

    Removed any inline styling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-
+	// Shows mobile navigation
 	$( "#mobile-icon" ).click(function() {
 		$( "#mobile-icon" ).hide( );
 		$( "#mobile-exit-cont" ).show( );
 		$( "nav" ).show( "slow" );
 	});
+	// Hides mobile navigation
 	$( "#mobile-exit-icon" ).click(function() {
 		$( "#mobile-icon" ).show( );
 		$( "#mobile-exit-cont" ).hide( );
 		$( "nav" ).hide( "slow" );
 	});	
-
 
 });
